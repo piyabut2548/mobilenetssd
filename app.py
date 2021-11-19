fdb5a64bf2dfc08d00d1d3d941fda36e86b4775c
piyabut2548 <89003757+piyabut2548@users.noreply.github.com>
2021-11-19 11:49:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,13 +151,37 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if (msg == "สวัสดี") :
             replyObj = TextSendMessage(text="ดีด้วย")
+            ine_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "อยู่ไหน") :
             replyObj = TextSendMessage(text="บ้าน")
+            ine_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "กินข้าวไหม") :
             replyObj = TextSendMessage(text="กิน")
+            ine_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "ไปไหน") :
             replyObj = TextSendMessage(text="นอน")
+            ine_bot_api.reply_message(rtoken, replyObj)
         else :
+            headers = request.headers
+            json_headers = json.dumps({k:v for k, v in headers.items()})
+            '''
+            json_line = request.get_json(force=False,cache=False)
+            json_line = json.dumps(json_line)
+            decoded = json.loads(json_line)
+            '''
+            crl= pycurl.Curl()
+            crl.setopt( crl.URL, "https://bots.dialogflow.com/line/k--jomf/webhook%22)
+            crl.setopt( crl.POST, 1)
+            #crl.setopt( crl.BINARYTRANSFER, true)
+            #crl.setopt( crl.POSTFIELDS, json_headers)
+            #crl.setopt( crl.HTTPHEADER, json_headers)
+            #crl.setopt( crl.SSL_VERIFYHOST, 2)
+            #crl.setopt( crl.SSL_VERIFYPEER, 1)
+            #crl.setopt( crl.FOLLOWLOCATION, 1)
+            #crl.setopt( crl.RETURNTRANSFER, 1)
+            crl.perform()
+            crl.close()
+                       
             replyObj = TextSendMessage(text=msg)
         line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
