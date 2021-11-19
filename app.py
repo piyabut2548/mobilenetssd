fc4fb3172858fc0e28280eaf54faa4df1d7d64e0
piyabut2548 <89003757+piyabut2548@users.noreply.github.com>
2021-11-19 11:52:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,16 +151,16 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if (msg == "สวัสดี") :
             replyObj = TextSendMessage(text="ดีด้วย")
-            ine_bot_api.reply_message(rtoken, replyObj)
+            line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "อยู่ไหน") :
             replyObj = TextSendMessage(text="บ้าน")
-            ine_bot_api.reply_message(rtoken, replyObj)
+            line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "กินข้าวไหม") :
             replyObj = TextSendMessage(text="กิน")
-            ine_bot_api.reply_message(rtoken, replyObj)
+            line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "ไปไหน") :
             replyObj = TextSendMessage(text="นอน")
-            ine_bot_api.reply_message(rtoken, replyObj)
+            line_bot_api.reply_message(rtoken, replyObj)
         else :
             headers = request.headers
             json_headers = json.dumps({k:v for k, v in headers.items()})
