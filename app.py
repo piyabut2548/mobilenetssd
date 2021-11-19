7232a4bb9d543b264856ef180d7481326e9e485f
piyabut2548 <89003757+piyabut2548@users.noreply.github.com>
2021-11-19 12:14:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ def event_handle(event):
             decoded = json.loads(json_line)
             '''
             crl= pycurl.Curl()
-            crl.setopt( crl.URL, "https://bots.dialogflow.com/line/l--bygk/webhook")
+            crl.setopt( crl.URL, "https://bots.dialogflow.com/line/l-bygk/webhook")
             crl.setopt( crl.POST, 1)
             #crl.setopt( crl.BINARYTRANSFER, true)
             #crl.setopt( crl.POSTFIELDS, json_headers)
