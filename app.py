a156c3b6bf4ea888cd29d70e7dfe9875a1a23664
piyabut2548 <89003757+piyabut2548@users.noreply.github.com>
2022-01-07 10:50:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,6 @@ def event_handle(event):
         line_bot_api.reply_message(rtoken, replyObj)
         return ''
 
-  
     if msgType == "text":
         msg = str(event["message"]["text"])
         if (msg == "สวัสดี") :
@@ -161,29 +160,18 @@ def event_handle(event):
         elif (msg == "ไปไหน") :
             replyObj = TextSendMessage(text="นอน")
             line_bot_api.reply_message(rtoken, replyObj)
+             elif msg == "covid" :
+            url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
+            response = requests.get(url)
+            response = response.json()
+            replyObj = TextSendMessage(text=str(response))
+            line_bot_api.reply_message(rtoken, replyObj)
         else :
             headers = request.headers
             json_headers = json.dumps({k:v for k, v in headers.items()})
-            '''
-            json_line = request.get_json(force=False,cache=False)
-            json_line = json.dumps(json_line)
-            decoded = json.loads(json_line)
-            '''
-            crl= pycurl.Curl()
-            crl.setopt( crl.URL, "https://bots.dialogflow.com/line/l-bygk/webhook")
-            crl.setopt( crl.POST, 1)
-            #crl.setopt( crl.BINARYTRANSFER, true)
-            #crl.setopt( crl.POSTFIELDS, json_headers)
-            #crl.setopt( crl.HTTPHEADER, json_headers)
-            #crl.setopt( crl.SSL_VERIFYHOST, 2)
-            #crl.setopt( crl.SSL_VERIFYPEER, 1)
-            #crl.setopt( crl.FOLLOWLOCATION, 1)
-            #crl.setopt( crl.RETURNTRANSFER, 1)
-            crl.perform()
-            crl.close()
-                       
-            replyObj = TextSendMessage(text=msg)
-            line_bot_api.reply_message(rtoken, replyObj)
+              json_headers.update({'Host':'bots.dialogflow.com'})
+            url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/0a0c1353-cd4a-4fcb-a207-33e08f3b7bb4"
+            requests.post(url,data=json_line, headers=json_headers)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
